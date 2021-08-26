7d5529bae47de0f8a3a36e8b2b990a05ad0f3541
Faisa Alhassan <devfalhassan@gmail.com>
2021-08-26 11:14:01 +0100

    Solved challenge 4

---
+++ 7KYU/4-objectKeyValueString.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+
+Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+Example:
+
+solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+
+*/
+
+function solution(pairs) {
+  const objectAndValue = [];
+
+  for (const key in pairs) {
+    objectAndValue.push(`${key} = ${pairs[key]}`);
+  }
+
+  return objectAndValue.join();
+}
+
+solution({ a: 1, b: '2' }); // should return "a = 1,b = 2"
