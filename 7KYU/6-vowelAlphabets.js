ba0aa2cb18dee837a51623a21e067103dd12a5b5
Faisa Alhassan <devfalhassan@gmail.com>
2021-08-27 12:10:53 +0100

    Solve challenge 6 under 7kyu

--- 7KYU/6-vowelAlphabets.js
+++ 7KYU/6-vowelAlphabets.js
@@ -8,3 +8,12 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 
  */
+
+function getCount(str) {
+  let vowelsCount = 0;
+  const vowel = ['a', 'e', 'i', 'o', 'u'];
+  str.split('').forEach((el) => vowel.includes(el) && vowelsCount++);
+  return vowelsCount;
+}
+
+console.log(getCount('aeiouw'));
